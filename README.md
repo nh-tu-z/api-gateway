e4785a8e107dce5f20501974ad4fb645c51a9efc
Tu Ngo <nhtu1611@gmail.com>
2023-01-02 09:41:42 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+API Gateway
+===========
+
+## Functionality overview
+    * Authorization: Session management - `express-session`
+    * Attack protection: Rate limiting (DDoS/DoS) - `express-rate-limit`, CORS - `cors`, `helmet`
+    * Log transactions - `express-winston`
+    * Proxying - `http-proxy-middleware`
+    * Load balancing - `TODO`
+
+## Availability
+
+### Authorization
+- TODO
+
+### Attack protection
+- TODO
+
+### Log transactions
+- TODO
+
+### Proxying
+- TODO
+
+### Load balancing
+- TODO
+
+## Usage
+
+### Environment configuration
+- `SESSION_SECRET`: TODO - description
---
+++ index.js
@@ -0,0 +1,91 @@
+const express = require("express");
+const session = require("express-session");
+const rateLimit = require("express-rate-limit");
+const winston = require("winston");
+const expressWinston = require("express-winston");
+const responseTime = require("response-time");
+const cors = require("cors");
+const helmet = require("helmet");
+require("dotenv").config();
+
+const app = express();
+const port = 3000;
+
+const secret = process.env.SESSION_SECRET;
+const store = new session.MemoryStore();
+const protect = (req, res, next) => {
+  const { authenticated } = req.session;
+
+  if (!authenticated) {
+    res.sendStatus(401);
+  } else {
+    next();
+  }
+};
+
+app.use(
+  session({
+    secret,
+    resave: false,
+    saveUninitialized: true,
+    store,
+  })
+);
+app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 5, // 5 calls
+    })
+);  
+
+app.use(responseTime());
+
+app.use(
+  expressWinston.logger({
+    transports: [new winston.transports.Console()],
+    format: winston.format.json(),
+    statusLevels: true,
+    meta: false,
+    msg: "HTTP {{req.method}} {{req.url}} {{res.statusCode}} {{res.responseTime}}ms",
+    expressFormat: true,
+    ignoreRoute() {
+      return false;
+    },
+  })
+);
+
+app.use(cors());
+app.use(helmet());
+
+app.get("/", (req, res) => {
+  const { name = "user" } = req.query;
+  res.send(`Hello ${name}!`);
+});
+
+app.get("/login", (req, res) => {
+  const { authenticated } = req.session;
+
+  console.log(req.session)
+
+  if (!authenticated) {
+    req.session.authenticated = true;
+    res.send("Successfully authenticated");
+  } else {
+    res.send("Already authenticated");
+  }
+});
+
+app.get("/logout", protect, (req, res) => {
+  req.session.destroy(() => {
+    res.send("Successfully logged out");
+  });
+});
+
+app.get("/protected", protect, (req, res) => {
+  const { name = "user" } = req.query;
+  res.send(`Hello ${name}!`);
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
